2320c9700373919d7c31f15d251a5be4ca8e298f
Morgan Roux <roux.morgan@gmail.com>
2020-12-26 22:27:40 +0100

    clean

--- srcs/exec/commands.c
+++ srcs/exec/commands.c
@@ -27,9 +27,7 @@ int     exec_command(t_command *cmd, char *envp[])
     char    *bin;
     int     pid;
     int     status;
-    int     fd;
 
-    fd = ft_lstsize(cmd->files_in) > 0 ? cmd->fd_in[0] : -1;
     if (cmd->exec == NULL)
         return (1);
     bin = find_bin(cmd->exec, envp);
@@ -40,11 +38,11 @@ int     exec_command(t_command *cmd, char *envp[])
     else if (pid == 0)
     {
         dup2(cmd->fd_in[0], STDIN_FILENO);
-        close(fd);
+        close(cmd->fd_in[0]);
         execve(bin, params, envp);
         return -1;
     }
-    close(fd);
+    close(cmd->fd_in[0]);
     waitpid(pid, &status, 0);
     free(bin);
     return (1);
