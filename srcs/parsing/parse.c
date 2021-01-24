454b4420dcc5cb1c60045898ec49779096113cae
Morgan Roux <roux.morgan@gmail.com>
2021-01-24 17:55:07 +0100

    add 0 and found litlle bug in

--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -79,6 +79,11 @@ char    *extract_var_name(char *str)
 
     str++;
     i = 0;
+    if(*str == '?')
+    {
+        name = ft_strdup("?");
+        return (name);
+    }
     while(str[i] != 0 && ft_isalnum(str[i]) == 1)
         i++;
     if (!(name = (char *) malloc(sizeof(char) * (i + 1))))
@@ -93,19 +98,22 @@ int     replace_var(char **str, char **start, t_command *global_command)
     char    *var_value;
     char    *new;
 
-    if (!ft_isalnum(*(*str + 1)))
+    if ((!ft_isalnum(*(*str + 1)) && (*(*str + 1) !=  '?')))
     {
         *str = *str + 1;
         return (0);
     }
     var_name = extract_var_name(*str);
-    var_value = get_var(global_command->env_arr, var_name);
+    if (*var_name == '?')
+        var_value = ft_itoa(global_command->ret);
+    else
+        var_value = get_var(global_command->env_arr, var_name);
     if (!(new = (char *) malloc(sizeof(char) * (ft_strlen(*start) - ft_strlen(var_name + 1) + ft_strlen(var_value)))))
         return (-1);
     ft_strlcpy(new, *start, *str  - *start + 1);
     ft_strlcat(new, var_value, ft_strlen(new) + ft_strlen(var_value) + 1);
     ft_strlcat(new, *str + (ft_strlen(var_name) + 1), ft_strlen(*start) + ft_strlen(var_value));
-    *str = new + ((*str - *start)  + ft_strlen(var_value));
+    *str = new + ((*str - *start)  + ft_strlen(var_value) - 1);
     free(*start);
     *start = new;
     free(var_value);
@@ -121,9 +129,9 @@ void    skip_squotes(char **str)
     (*str)++;
     while (**str != 0)
     {
-        if (**str == '\\' && escape == 0)
+        if ((**str == '\\') && (escape == 0))
             escape = 1;
-        else if (**str == '\'' && escape == 0)
+        else if ((**str == '\'') && (escape == 0))
             return ;
         else if (escape == 1)
             escape = 0;
