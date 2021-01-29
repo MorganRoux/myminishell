0e70abe9ea322667c2eb334296ab392579ab9e6c
Morgan Roux <roux.morgan@gmail.com>
2021-01-29 14:57:12 +0100

    norm get_next_line

--- srcs/get_next_line/get_next_line.c
+++ srcs/get_next_line/get_next_line.c
@@ -13,6 +13,28 @@
 #include "minishell.h"
 #include <stdio.h>
 
+int     get_next_line_loop(t_fl *fl, t_command *mimi, char **line)
+{
+    int ln;
+
+    ln = 0;
+    while ((ln = find_line(fl)) == -1)
+	{
+		*line = ft_strnjoin(*line, fl->buffer + fl->pos, fl->bytes_read - fl->pos);
+		fl->pos = 0;
+		if ((fl->bytes_read = read(fl->fd, fl->buffer, BUFFER_SIZE)) < 0)
+		{
+			if (fl->bytes_read == 0 && *line == NULL)
+				close_mimi(mimi, 1);
+			reinit(fl);
+			return (fl->bytes_read);
+		}
+	}
+	*line = ft_strnjoin(*line, fl->buffer + fl->pos, ln);
+	fl->pos = (fl->pos + ln + 1 >= fl->bytes_read) ? 0 : fl->pos + ln + 1;
+	return (1);
+}
+
 int		get_next_line(int fd, char **line, t_command *mimi)
 {
 	static t_fl		fl;
@@ -28,19 +50,5 @@ int		get_next_line(int fd, char **line, t_command *mimi)
 			close_mimi(mimi, 1);
 		return (fl.bytes_read);
 	}
-	while ((ln = find_line(&fl)) == -1)
-	{
-		*line = ft_strnjoin(*line, fl.buffer + fl.pos, fl.bytes_read - fl.pos);
-		fl.pos = 0;
-		if ((fl.bytes_read = read(fl.fd, fl.buffer, BUFFER_SIZE)) < 0)
-		{
-			if (fl.bytes_read == 0 && *line == NULL)
-				close_mimi(mimi, 1);
-			reinit(&fl);
-			return (fl.bytes_read);
-		}
-	}
-	*line = ft_strnjoin(*line, fl.buffer + fl.pos, ln);
-	fl.pos = (fl.pos + ln + 1 >= fl.bytes_read) ? 0 : fl.pos + ln + 1;
-	return (1);
+	return (get_next_line_loop(&fl, mimi, line));
 }
