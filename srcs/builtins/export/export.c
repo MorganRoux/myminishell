479983f6e9e3c94a16352eb4ea6727c7e2df7c77
Morgan@VM <mroux@student.42.fr>
2021-02-28 20:22:31 +0100

    export with variable already exist

--- srcs/builtins/export/export.c
+++ srcs/builtins/export/export.c
@@ -1,5 +1,44 @@
 #include "minishell.h"
 
+char    **delete_var(char *env[], int index)
+{
+    int     i;
+    int     j;
+    char    **new_env;
+
+    i = 0;
+    j = 0;
+    if (!(new_env = malloc(sizeof(char *) * get_strs_len(env))))
+        return NULL;
+    while (i < get_strs_len(env))
+    {
+        if (i != index)
+            new_env[j++] = ft_strdup(env[i]);
+        i++;
+    }
+    new_env[j] = 0;
+    return (new_env);
+}
+
+char    **delete_from_env(char *env[], char *var)
+{
+    int     index;
+    char    **var_splitted;
+    char    **new_env;
+
+    var_splitted = ft_split(var, '=');
+    index = get_var_index(env, var_splitted[0]);
+    if (index == -1)
+        new_env = env;
+    else
+    {
+        new_env = delete_var(env, index);
+        free_strs(env);
+    }
+    free_strs(var_splitted);
+    return (new_env);
+}
+
 char    **add_to_env(char *env[], char *var)
 {
     int     len;
@@ -34,6 +73,7 @@ int     export(t_command *global_command, t_command *cmd)
     {
         if (!check_export_arg(args->content))
             return (2);
+        global_command->env_arr  = delete_from_env(global_command->env_arr, args->content);
         global_command->env_arr = add_to_env(global_command->env_arr, args->content);
         args = args->next;
     }
