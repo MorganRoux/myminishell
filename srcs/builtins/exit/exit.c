435e3da51e83c6f2f7f8c0a04dbe1d24d62d79e1
Morgan@VM <mroux@student.42.fr>
2021-02-28 21:17:59 +0100

    exit with args

--- srcs/builtins/exit/exit.c
+++ srcs/builtins/exit/exit.c
@@ -1,12 +1,28 @@
 #include "minishell.h"
 
+void    exit_arg(t_list_str *arg)
+{
+    if ((ft_atoi(arg->content) == 0) && ((char *)(arg->content))[0] != '0')
+    {
+        ft_printf("exit: argument numérique nécessaire");
+        exit(2);
+    }
+    exit(ft_atoi(arg->content));
+}
 void    do_exit(t_command *global_command, t_command *cmd)
 {
     (void)cmd;
 
     //ft_printf("exit\n");
-    if (global_command->ret == 0)
+    if (ft_lstsize(cmd->args) > 1)
+        ft_printf("exit: too many arguments");
+    else if (ft_lstsize(cmd->args) == 1)
+        exit_arg(cmd->args);
+    else
+    {
+        if (global_command->ret == 0)
         exit(0);
-    errno = global_command->ret;
-    exit(errno);
+        errno = global_command->ret;
+        exit(errno);
+    }
 }
